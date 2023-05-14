5ba5c1ab25e19ecd68d1a92b66e26ce193793abf
Linus <124461237+linus549@users.noreply.github.com>
2023-05-14 14:31:56 +0200

    rework how NotificationsToggle component manages Notifications API

--- src/components/Settings/NotificationsToggle.js
+++ src/components/Settings/NotificationsToggle.js
@@ -1,54 +1,57 @@
+import { useState } from "react";
+import uniqueId from "lodash/uniqueId";
 import Toggle from "components/Settings/Toggle";
 
 function NotificationsToggle({ checked, onChange }) {
-  function handleChange(e) {
-    if (Notification.permission === "default") {
-      if (isPromiseSupported()) {
-        Notification.requestPermission().then(() => handlePermission(e));
-      } else {
-        Notification.requestPermission(() => handlePermission(e));
-      }
-    } else {
-      onChange(e);
+  const [key, setKey] = useState(uniqueId());
+
+  function handleClick() {
+    try {
+      Notification.requestPermission().then(handlePermission);
+    } catch (error) {
+      // if Promise not supported
+      Notification.requestPermission(handlePermission);
     }
 
-    function handlePermission(e) {
-      e.target.checked = Notification.permission === "granted";
-      onChange(e);
+    function handlePermission() {
+      setKey(uniqueId());
     }
   }
 
   return (
     <Toggle
+      key={key}
       id="notifications"
       name="notifications"
       label="Notifications"
       checked={checked}
       disabled={!isNotificationAvailable}
-      onlyCheckboxDisabled={Notification.permission === "denied"}
-      onChange={handleChange}
+      onlyCheckboxDisabled={
+        isNotificationAvailable && Notification.permission !== "granted"
+      }
+      onChange={onChange}
     >
-      {isNotificationAvailable
-        ? `Permission: ${
-            Notification.permission === "default"
-              ? "not set"
-              : Notification.permission
-          }`
-        : "Not available for your browser/device"}
+      {isNotificationAvailable ? (
+        <>
+          {Notification.permission === "default" ? (
+            <button
+              type="button"
+              className="toggle__permission-btn"
+              onClick={handleClick}
+            >
+              Request permission
+            </button>
+          ) : (
+            `Device permission: ${Notification.permission}`
+          )}
+        </>
+      ) : (
+        "Not available for your browser/device"
+      )}
     </Toggle>
   );
 }
 
 const isNotificationAvailable = "Notification" in window;
-
-function isPromiseSupported() {
-  try {
-    Notification.requestPermission().then();
-  } catch (error) {
-    return false;
-  }
-
-  return true;
-}
 
 export default NotificationsToggle;
